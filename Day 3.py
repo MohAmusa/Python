7a7f8073d1597f6d49afb30f8a8d27aba4547ff0
Mohammed <mohamusa27@gmail.com>
2024-04-03 08:54:51 +0100

    https://github.com/MohAmusa/Python-

---
+++ Day 3.py
@@ -0,0 +1,7 @@
+age = 28  # QUESTION 1
+h = 5.11  # QUESTION 2
+complex_number = 5 + 6j  # QUESTION 3
+base = float(input("Enter base:20"))  # QUESTION 4
+height = float(input("Enter height:10"))
+area = 0.5 * base * height
+print("The area of the triangle:", area)
